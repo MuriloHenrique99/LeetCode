5e9e4f381d23de6c5ea7b5de4bce7ee8d3660552
Ze das Couves <murilohenriquemendes99@gmail.com>
2026-09-17 13:08:49 -0300

    Palindromos em c++

---
+++ palindrome.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+bool isPalindrome(int x) {
+    
+    int teste = 0;
+
+    if(x < 0){
+        return false;
+    } else if(x % 10 == 0 && x != 0){
+        return false;
+    }
+
+    while(x > teste){
+        teste = (teste * 10) + (x % 10);
+        x = x / 10;
+    }
+
+    return x == teste || x == teste / 10;
+
+}
+
+int main(){
+    
+    int num;
+
+    for(int i = 0; i < 10; ++i){
+        cout << "Digite o numero para o teste: "; cin >> num;
+        bool result = isPalindrome(num);
+        cout << boolalpha << result << endl;
+    }
+}
